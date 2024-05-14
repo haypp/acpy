47bc5dbc2b36f98f98d10610345826ea153fee25
haypp <reyjuna.al@gmail.com>
2024-05-14 21:47:42 +0700

    menambahkan timer

---
+++ timeKU.py
@@ -0,0 +1,25 @@
+from datetime import datetime, time
+
+# from time import sleep
+
+def adjust_temp(_up_votes,_down_votes,_tot_votes):
+    _tot_votes = int(_tot_votes)
+    _up_votes = int(_up_votes)
+    _down_votes = int(_down_votes)
+    if _tot_votes > (_up_votes + _down_votes):
+            if _up_votes > _down_votes:
+                print('send up')
+            else:
+                print('send down')
+    else:
+        print('reset list')
+
+def set_off(_time_end):
+    target_time = time(*map(int, _time_end.split(':')))
+    current_time = datetime.now().time()
+    
+    while current_time < target_time:
+        current_time = datetime.now().time()
+    
+    # Execute the desired function here
+    print('stopnow')
--- webserver.py
+++ webserver.py
@@ -2,14 +2,12 @@ from flask import Flask, render_template
 from flask.cli import F
 import netifaces as ni
 import datetime
-from temp import get_suhu
+# from temp import get_suhu
 
 app = Flask(__name__)
 
-ipaddr = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
-#ipaddr = ni.ifaddresses('wlp3s0')[ni.AF_INET][0]['addr']
-
-# Dummy temperature value
+# ipaddr = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
+ipaddr = ni.ifaddresses('wlp3s0')[ni.AF_INET][0]['addr']
 
 # Function to get current time
 def get_current_time():
@@ -19,7 +17,8 @@ def get_current_time():
 def index():
     # Get current time
     # current_time = get_current_time()
-    return render_template('index.html', temperature=get_suhu(4))
+    # return render_template('index.html', temperature=get_suhu(4))
+    return render_template('index.html', temperature=5)
 
 if __name__ == '__main__':
     from threading import Thread
