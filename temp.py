f68adc28390f8883b2945b2d04e81988a7d44e3a
haypp <reyjuna.al@gmail.com>
2024-06-04 13:47:42 +0700

    just return temp only

--- temp.py
+++ temp.py
@@ -4,6 +4,7 @@ def get_suhu(gpio_pin):
     sensor = Adafruit_DHT.DHT22
     humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio_pin)
     if humidity is not None and temperature is not None:
-        return 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
+        # return 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity)
+        return '{0:0.1f}'.format(temperature)
     else:
         return 'Failed to get reading. Try again!'
