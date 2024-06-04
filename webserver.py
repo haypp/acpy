c76e19b08b4c45437e4b2be42c5bafd9f7c5f655
haypp <reyjuna.al@gmail.com>
2024-06-04 13:48:19 +0700

    small adjustment

--- webserver.py
+++ webserver.py
@@ -1,22 +1,23 @@
+# tidak menyiapkan program untuk counter.
 from flask import Flask, render_template
 from flask.cli import F
 import netifaces as ni
 import datetime
-# from temp import get_suhu
+from temp import get_suhu
 
 app = Flask(__name__)
 
-# ipaddr = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
-ipaddr = ni.ifaddresses('wlp3s0')[ni.AF_INET][0]['addr']
+ipaddr = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
+# ipaddr = ni.ifaddresses('wlp3s0')[ni.AF_INET][0]['addr']
 
 # Function to get current time
-def get_current_time():
-    return datetime.datetime.now().strftime("%H:%M")
+# def get_current_time():
+    # return datetime.datetime.now().strftime("%H:%M")
 
 @app.route('/')
 def index():
-    # return render_template('index.html', temperature=get_suhu(4))
-    return render_template('index.html', temperature=5)
+    return render_template('index.html', temperature=get_suhu(4))
+    # return render_template('index.html', temperature=5)
 
 if __name__ == '__main__':
     from threading import Thread
